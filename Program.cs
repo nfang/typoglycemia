4824d437c064e57643ef1a6b137e87339ccf01a5
Nathan Fang <nathan.fang@icloud.com>
2017-07-01 01:08:29 +1000

    :tada: initial commit

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace typoglycemia
+{
+    class Program
+    {
+        static string GenerateTypoglycemia(string text)
+        {
+            char[] seq = text.ToCharArray();
+            int len = seq.Length;
+            int word_start = 0;
+            var rand = new Random();
+
+            for (int i = 0; i < len; i++)
+            {
+                if (!Char.IsLetter(seq[i]))
+                {
+                    int shuffle_start = word_start + 1,
+                        k = i - 2;
+                    
+                    while (k > shuffle_start)
+                    {
+                        var j = rand.Next(shuffle_start, k);
+                        seq[k] ^= seq[j];
+                        seq[j] ^= seq[k];
+                        seq[k] ^= seq[j];
+                        k--;
+                    }
+
+                    word_start = i + 1;
+                }
+            }
+
+            return new String(seq);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a paragraph:\n");
+            var text = Console.ReadLine();
+            Console.WriteLine("\nResult:\n");
+            Console.WriteLine(GenerateTypoglycemia(text));
+            Console.ReadLine();
+        }
+    }
+}
